e81561b7cf81f6491f1cf334e15dfb08f258851d
Tomasz Osuch <tomasz.osuch@gmail.com>
2021-03-09 13:14:45 +0100

    first commit

---
+++ src/main/java/com/kodilla/library/RestLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestLibraryApplication.class, args);
+    }
+
+}
